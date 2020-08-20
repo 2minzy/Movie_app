11858b63c2ffd2df49ab45edc9059c60e81006cd
Minji <minjilee0423@gmail.com>
2020-08-21 01:32:36 +0900

    Added Landing-page

---
+++ client/src/components/Config.js
@@ -0,0 +1,6 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const API_KEY = 'e746635f27e32cc1635c959a093766ec';
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
